125ef06185ae136a24439ccc793c65fa100cf9ad
pp-3 <himani.r@prominentpixel.com>
2023-08-21 18:38:50 +0530

    Completed Excercise 1 and 2.

---
+++ Day2/Excercise2/basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,54 @@
+//Defined Funtion
+function add(num1, num2) {
+    const result = num1 + num2;
+    // alert(`The result of Addition of ${num1} and ${num2} is:` + result);
+
+    return result;
+}
+
+//now I'm calling the function
+// add(12,34);
+
+//use const variable 
+// const currentResult = add(12,34);
+
+// currentResult = (currentResult + 10) * 3 / 2 - 1; //using Arithmatic Operators
+// here using backticks
+// let calculationDescription = `(${currentResult} + 10) * 3 / 2 - 1`; //template literal
+
+/* here did some string operation
+ let errormessage = `This is my erro message  
+
+ whichi want to show`;
+
+ let errormessage = "This is my erro message"+
+
+ "which want to show";
+
+ let errormessage = "This is my erro message \n"+
+
+ "which want to show"; */
+
+//function call
+// outputResult(currentResult, calculationDescription);
+
+
+let currentResult = 0;
+
+function add() {
+     
+    currentResult = currentResult + parseInt(userInput.value);//here using type conversation
+    outputResult(currentResult, " ");
+
+}
+
+
+
+
+// addEventListener
+
+addBtn.addEventListener("click",add) //addEvenListener take two arguments
+
+
+//for understanding Function
+
