878ff15ea3c23eb739550061de806090491b25ed
pp-3 <himani.r@prominentpixel.com>
2023-08-28 19:34:57 +0530

    Completed Day6

---
+++ Day6/Exercise6/Q2.js
@@ -0,0 +1,13 @@
+// Question2:
+// Find the sum of all elements of a given array? Ex. like arr = [1, 6, 17, 70, 85];    
+
+
+const arr = [1,6,17,70,85];
+let sum = 0;
+
+
+for(let i of arr){
+    sum+=i
+}
+
+console.log(sum);
