be1655261e59307ad311b0c622b5b0441782fd9a
pp-3 <himani.r@prominentpixel.com>
2023-09-05 19:23:59 +0530

    Today compeleted first evalution

--- Day9/Practice/Practice Project/assets/scripts/analytics.js
+++ Day9/Practice/Practice Project/assets/scripts/analytics.js
@@ -1,1 +1,9 @@
-console.log("analytics starts.........")
+const intervalId = setInterval(() => {
+    console.log("analytics starts.........");
+},2000); 
+
+document.getElementById('stop-analytics-btn').addEventListener('click',() => {
+
+    clearInterval(intervalId)
+
+})
